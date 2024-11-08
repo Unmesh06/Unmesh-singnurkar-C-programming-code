22cf9c66f6d012abc753280ecc466d7378c51d79
Unmesh06 <singnurkaru06@gmail.com>
2024-11-08 14:22:45 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,11 @@
+//Program for array
+//Program for changing number
+#include<stdio.h>
+int main()
+{
+    int num[]={21,33,24,32};
+    //Replace Value at index 2 with 45
+    num[2]=45;
+    printf("Number at index 2 after replacing will be: %d",num[2]); //Output will be Number at index 2 after replacing will be:45
+    return 0;
+}
